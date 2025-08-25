300c9063433e5d110c86b34faedf05458a85a522
IgorUlian <i97ulian@gmail.com>
2025-08-25 09:47:26 -0300

    fix: Limpar caches na inicialização do container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,6 +3,11 @@
 # Mude para o diretório da aplicação
 cd /var/www/html
 
+# Limpa os caches antigos para garantir que as novas configurações e views sejam usadas
+php artisan config:clear
+php artisan view:clear
+php artisan route:clear
+
 # Rode as migrations do banco de dados
 php artisan migrate --force
 
